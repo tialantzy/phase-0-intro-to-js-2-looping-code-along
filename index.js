94344922274defa19c29d20d582c199e13042884
tialantzy <tialantzy@gmail.com>
2022-04-28 21:57:01 -0500

    commit tree 504aebf027271ed100b4c55b55c0385f79625096

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+    let messages = []; 
+    for (let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+return messages; 
+}
+
+function countDown(down) {
+    let counter = 10
+    while (counter > -1 ) {
+        console.log(counter--)
+    }
+}
